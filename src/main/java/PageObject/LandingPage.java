076f9345360b46b9cc4e26fca2287beb2aeba5a1
vivekanandd1 <vivekanandd1@live.com>
2022-06-19 23:36:41 +0530

    Assertion of Testng

--- src/main/java/PageObject/LandingPage.java
+++ src/main/java/PageObject/LandingPage.java
@@ -7,13 +7,25 @@ import org.openqa.selenium.WebElement;
 public class LandingPage {
 	
 	public WebDriver driver;
+	By Signin = By.cssSelector("i.fa.fa-lock.fa-lg");
 	By emailbox = By.xpath("//input[@name='email']");
 	By PasswordBox = By.xpath("//input[@name=' password']");
 	By signInBtn = By.xpath("//input[@type='submit']");
+	By TextPrsnt = By.xpath("//h2[normalize-space()='Featured Courses']");
 	
 	public LandingPage(WebDriver driver) {
 		this.driver=driver;
 	}
+	
+	public WebElement Signin() {
+		return driver.findElement(Signin);
+	}
+	
+	public WebElement Title() {
+		return driver.findElement(TextPrsnt);
+	}
+	
+	
      
 	public WebElement Mailid() {
 		return driver.findElement(emailbox);
--- src/main/java/TestNGFrameWork/NewProj/HomePage.java
+++ src/main/java/TestNGFrameWork/NewProj/HomePage.java
@@ -3,8 +3,10 @@ package TestNGFrameWork.NewProj;
 import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import PageObject.LandingPage;
@@ -16,14 +18,33 @@ public class HomePage extends Base {
 	WebDriver driver;
 
 	
+//	@Test(dataProvider="getdata")TestNGFrameWork.NewProj.HomePage
 	@Test
+//	public void BasePageNaviagtion(String Username,String Password,String Text) throws IOException  {
 	public void BasePageNaviagtion() throws IOException  {
 	    driver=DriverInitializer();
 	    LandingPage landingpage=new LandingPage(driver);
-		driver.get("https://rahulshettyacademy.com/sign_in/");
-		landingpage.Mailid().sendKeys("vivekanadd1@live.com");
-		landingpage.Passwrd().sendKeys("Rony@11888");//Rony@11888
-		landingpage.Getlogin().click();
+		driver.get("http://www.qaclickacademy.com/");
+//		landingpage.Signin().click();
+//		landingpage.Mailid().sendKeys(Username);
+//		landingpage.Passwrd().sendKeys(Password);
+//		System.out.println(Text);
+//		landingpage.Getlogin().click();
+		landingpage.Title().getText();
+		Assert.assertEquals(landingpage.Title().getText(), "FEATURED COURSES");
 	}
+	
+//	@DataProvider
+//	public Object[][] getdata() {
+//		Object[][] data = new Object[2][3];
+//		data[0][0] = "vivekanandd1@live.com";
+//		data[0][1] = "Rony@11888";
+//		data[0][2] = "Login Completed";
+//		
+//		data[1][0] = "fdsj@fjd.com";
+//		data[1][1] = "Rfd11888";
+//		data[1][2] = "second loop";
+//		return data;
+//	}
 	
 }
